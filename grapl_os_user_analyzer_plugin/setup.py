b9a44894c37c48b18afc2a5816a5f4e6cb1acf4a
Insanitybit <insanitybit@gmail.com>
2019-11-16 17:50:10 -0800

    Fix up module

--- grapl_os_user_analyzer_plugin/setup.py
+++ grapl_os_user_analyzer_plugin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="grapl_os_user_analyzer_plugin",
-    version="0.1.3",
+    version="0.1.4",
     description="OS User Plugin for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl-os-user-plugin/",
     author="insanitybit",
